5aa4be01a6b6338454da50657484394e239621f7
Murat Can Kurt <kurt.muratcan@gmail.com>
2022-04-15 20:51:45 +0300

    Configrue db connection and rename Identity tables. Add person entity

---
+++ Data/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BuildingSiteManagementWebApp.Data.Entities
+{
+    public class Person : IdentityUser
+    {
+        public string NationalId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
